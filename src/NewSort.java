e89dda88890a2eb77cddce7a49953fc0cf844d56
vamsi <soman@LAPTOP-S45VTFHL>
2021-02-11 22:40:07 +0530

    NewSort

--- src/NewSort.java
+++ src/NewSort.java
@@ -15,7 +15,7 @@ public class NewSort {
 
 	public static void main(String[] args) {
 		
-		 int[] arr1 = {20,30,88,11,5,36,49,78,25};  
+		 int[] arr1 = { 20,30,88,11,5,36,49,78,25 };  
 	        System.out.println("Array Before Unsorted of Selection Sort");  
 	        for(int i:arr1){  
 	            System.out.print(i+" ");  
